051ed0e08ccf7f1d524841bbee44c96700455fa7
Maicon de Menezes <maicon.menezes@celero.com.br>
2024-11-19 09:34:02 -0300

    REMOVE: CompanyService implementation and refactor client to use CompanyServiceStub

--- app/client.py
+++ app/client.py
@@ -2,7 +2,7 @@
 import grpc
 
 # Internal imports
-from app.services.company import CompanyService
+from app.protos.company_pb2_grpc import CompanyServiceStub
 from app.protos.company_pb2 import (
     CreateCompanyRequest,
     DeactivateCompanyRequest,
@@ -11,40 +11,40 @@ from app.protos.company_pb2 import (
 
 
 class CompanyClient:
-    def __init__(self, address, token):
-        self.token = token
-        self.channel = self.create_channel(address)
-        self.service = self.create_service()
+    def __init__(self, address):
+        self.address = address
+        self.channel = None
+        self.stub = None
 
-    def create_channel(self, address):
-        call_credentials = grpc.access_token_call_credentials(self.token)
+    def _create_channel(self, token):
+        call_credentials = grpc.access_token_call_credentials(token)
         channel_credentials = grpc.ssl_channel_credentials()
         composite_credentials = grpc.composite_channel_credentials(
             channel_credentials, call_credentials
         )
-        return grpc.secure_channel(address, composite_credentials)
-
-    def create_service(self):
-        return CompanyService(self.channel)
+        self.channel = grpc.secure_channel(self.address, composite_credentials)
+        self.stub = CompanyServiceStub(self.channel)
 
     def create_company(
         self,
+        token,
         company_id,
         company_name,
         trade_name,
-        registration_number,
+        legal_entity_registration,
     ):
+        self._create_channel(token)
         request = CreateCompanyRequest(
             company_id=company_id,
             company_name=company_name,
             trade_name=trade_name,
-            registration_number=registration_number
+            legal_entity_registration=legal_entity_registration
         )
-        response_stream = self.stub.CreateCompany(request)
-        for response in response_stream:
-            yield response
+        response = self.stub.CreateCompany(request)
+        return response
 
-    def deactivate_company(self, company_id, reason):
+    def deactivate_company(self, token, company_id, reason):
+        self._create_channel(token)
         request = DeactivateCompanyRequest(
             company_id=company_id,
             reason=reason
@@ -52,11 +52,11 @@ class CompanyClient:
         response = self.stub.DeactivateCompany(request)
         return response
 
-    def reactivate_company(self, company_id, reason):
+    def reactivate_company(self, token, company_id, reason):
+        self._create_channel(token)
         request = ReactivateCompanyRequest(
             company_id=company_id,
             reason=reason
         )
-        response_stream = self.stub.ReactivateCompany(request)
-        for response in response_stream:
-            yield response
+        response = self.stub.ReactivateCompany(request)
+        return response
